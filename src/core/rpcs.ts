422e9d6b00be10d9c471c21e5714f1be59e33dc4
Minh Hieu Tran <hieutm.site@gmail.com>
2025-11-26 17:26:01 +0700

    chore(rpcs): add rpcs

--- src/core/rpcs.ts
+++ src/core/rpcs.ts
@@ -12,7 +12,8 @@ export const LENS_MAINNET_WS = [
 
 export const LENS_TESTNET_RPCS = [
   "https://rpc.testnet.lens.dev",
-  "https://lens-sepolia.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy"
+  "https://lens-sepolia.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy",
+  "https://lens-testnet.drpc.org"
 ];
 
 export const LENS_TESTNET_WS = [
