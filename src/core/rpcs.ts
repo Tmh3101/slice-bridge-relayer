84300f2aa0cd8bfaf126fe1a7e99dfc6b3889888
Minh Hieu Tran <hieutm.site@gmail.com>
2025-12-02 16:04:06 +0700

    chore: fix rpcs

--- src/core/rpcs.ts
+++ src/core/rpcs.ts
@@ -1,7 +1,9 @@
 export const LENS_MAINNET_RPCS = [
+  "https://lens-mainnet.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy",
   "https://rpc.lens.xyz",
   "https://api.lens.matterhosted.dev",
-  "https://lens-mainnet.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy"
+  "https://light-icy-dinghy.lens-mainnet.quiknode.pro",
+  "https://lens.drpc.org"
 ];
 
 export const LENS_MAINNET_WS = [
@@ -11,8 +13,8 @@ export const LENS_MAINNET_WS = [
 ];
 
 export const LENS_TESTNET_RPCS = [
-  "https://rpc.testnet.lens.dev",
   "https://lens-sepolia.g.alchemy.com/v2/N_HuqeYE3mr_enxw-BGFI2rOm1U7bhGy",
+  "https://rpc.testnet.lens.dev",
   "https://lens-testnet.drpc.org"
 ];
 
@@ -29,7 +31,9 @@ export const BSC_TESTNET_RPCS = [
   "https://data-seed-prebsc-1-s3.bnbchain.org:8545",
   "https://data-seed-prebsc-2-s3.bnbchain.org:8545",
   "https://bsc-testnet-rpc.publicnode.com",
-  "https://bsc-testnet.drpc.org"
+  "https://bsc-testnet.drpc.org",
+  "https://bnb-testnet.api.onfinality.io/public",
+  "https://api.zan.top/bsc-testnet"
 ];
 
 export const BSC_TESTNET_WS = [
@@ -38,24 +42,10 @@ export const BSC_TESTNET_WS = [
 ]
 
 export const BSC_MAINNET_RPCS = [
-  "https://bsc-dataseed.bnbchain.org",
-  "https://bsc-dataseed1.bnbchain.org",
-  "https://bsc-dataseed2.bnbchain.org",
-  "https://bsc-dataseed3.bnbchain.org",
-  "https://bsc-dataseed4.bnbchain.org",
   "https://bsc-rpc.publicnode.com",
   "https://bsc.drpc.org",
   "https://binance.llamarpc.com",
-  "https://bsc-dataseed.defibit.io",
-  "https://bsc-dataseed1.defibit.io",
-  "https://bsc-dataseed2.defibit.io",
-  "https://bsc-dataseed3.defibit.io",
-  "https://bsc-dataseed4.defibit.io",
-  "https://bsc-dataseed.nariox.org",
-  "https://bsc-dataseed1.nariox.org",
-  "https://bsc-dataseed2.nariox.org",
-  "https://bsc-dataseed3.nariox.org",
-  "https://bsc-dataseed4.nariox.org",
+  "https://bsc-dataseed1.binance.org"
 ];
 
 export const BSC_MAINNET_WS = [
