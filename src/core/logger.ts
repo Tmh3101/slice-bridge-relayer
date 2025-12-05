75fdaf198bd891c60ded7d796cd9738313c3bbab
Minh Hieu Tran <hieutm.site@gmail.com>
2025-12-05 13:37:35 +0700

    log: add custorm log for prod

--- src/core/logger.ts
+++ src/core/logger.ts
@@ -4,14 +4,12 @@ const isDev = process.env.NODE_ENV !== 'production'
 
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'debug',
-  transport: isDev
-    ? {
-        target: 'pino-pretty',
-        options: {
-          colorize: true,
-          translateTime: 'HH:MM:ss',
-          ignore: 'pid,hostname',
-        },
-      }
-    : undefined,
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'HH:MM:ss',
+      ignore: isDev ? 'pid,hostname' : undefined,
+    },
+  },
 })
